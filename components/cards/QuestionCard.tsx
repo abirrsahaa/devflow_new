d4bf4024af861ac545a90c7499594c6f758ae5ab
abirrsahaa <abirsaha453@gmail.com>
2024-10-20 10:53:01 +0530

    some changes for implementing webhook using svix

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import RenderTag from '../shared/RenderTag';
 import Metric from '../shared/Metric';
 import { formatAndDivideNumber, getTimestamp } from '@/lib/utils';
-//
+import { SignedIn } from '@clerk/nextjs';
 
 interface QuestionProps {
   _id: string;
@@ -16,14 +16,17 @@ interface QuestionProps {
     _id: string;
     name: string;
     picture: string;
+    clerkId: string;
   };
-  upvotes: number;
+  upvotes: string[];
   views: number;
   answers: Array<object>;
   createdAt: Date;
+  clerkId?: string | null;
 }
 
 const QuestionCard = ({
+  clerkId,
   _id,
   title,
   tags,
@@ -33,12 +36,11 @@ const QuestionCard = ({
   answers,
   createdAt,
 }: QuestionProps) => {
+  const showActionButtons = clerkId && clerkId === author.clerkId;
+
   return (
     <div className="card-wrapper rounded-[10px] p-9 sm:px-11">
-      {/* question card is nothing but a div which has 3 div within */}
-      {/* the first div has the title of the question and additional edit/delete option for author and also timestamp for smaller devices  */}
       <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
-        {/* here the first is the title and the timestamp for smaller devices */}
         <div>
           <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
             {getTimestamp(createdAt)}
@@ -50,18 +52,22 @@ const QuestionCard = ({
           </Link>
         </div>
 
-        {/* here the second is the edit/delete option for the author himself  */}
+        {/* <SignedIn>
+          {showActionButtons && (
+            <EditDeleteAction type="Question" itemId={JSON.stringify(_id)} />
+          )}
+        </SignedIn> */}
       </div>
-      {/* second div has the tags of the question  */}
+
       <div className="mt-3.5 flex flex-wrap gap-2">
         {tags.map((tag) => (
           <RenderTag key={tag._id} _id={tag._id} name={tag.name} />
         ))}
       </div>
-      {/* while the third is for all possible information  */}{' '}
+
       <div className="flex-between mt-6 w-full flex-wrap gap-3">
         <Metric
-          imgUrl="/assets/icons/avatar.svg"
+          imgUrl={author.picture}
           alt="user"
           value={author.name}
           title={` - asked ${getTimestamp(createdAt)}`}
@@ -69,28 +75,29 @@ const QuestionCard = ({
           isAuthor
           textStyles="body-medium text-dark400_light700"
         />
-
-        <Metric
-          imgUrl="/assets/icons/like.svg"
-          alt="Upvotes"
-          value={formatAndDivideNumber(upvotes)}
-          title=" Votes"
-          textStyles="small-medium text-dark400_light800"
-        />
-        <Metric
-          imgUrl="/assets/icons/message.svg"
-          alt="message"
-          value={formatAndDivideNumber(answers.length)}
-          title=" Answers"
-          textStyles="small-medium text-dark400_light800"
-        />
-        <Metric
-          imgUrl="/assets/icons/eye.svg"
-          alt="eye"
-          value={formatAndDivideNumber(views)}
-          title=" Views"
-          textStyles="small-medium text-dark400_light800"
-        />
+        <div className="flex items-center gap-3 max-sm:flex-wrap max-sm:justify-start">
+          <Metric
+            imgUrl="/assets/icons/like.svg"
+            alt="Upvotes"
+            value={formatAndDivideNumber(upvotes.length)}
+            title=" Votes"
+            textStyles="small-medium text-dark400_light800"
+          />
+          <Metric
+            imgUrl="/assets/icons/message.svg"
+            alt="message"
+            value={formatAndDivideNumber(answers.length)}
+            title=" Answers"
+            textStyles="small-medium text-dark400_light800"
+          />
+          <Metric
+            imgUrl="/assets/icons/eye.svg"
+            alt="eye"
+            value={formatAndDivideNumber(views)}
+            title=" Views"
+            textStyles="small-medium text-dark400_light800"
+          />
+        </div>
       </div>
     </div>
   );
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,23 +1,96 @@
 import User from '@/database/user.model';
 import { connectToDatabase } from '../mongoose';
+import {
+  CreateUserParams,
+  DeleteUserParams,
+  GetUserByIdParams,
+  UpdateUserParams,
+} from './shared.types';
+import { revalidatePath } from 'next/cache';
+import Question from '@/database/question.model';
 
-export async function getUserById(params: any) {
+export async function getUserById(params: GetUserByIdParams) {
   try {
     connectToDatabase();
 
     console.log('the parms here is ', params);
 
-    const { clerkId } = params;
+    const { userId } = params;
 
-    console.log('clerkId', clerkId);
+    console.log('clerkId', userId);
 
     // idhar db call karo to check if the user exists
     const user = await User.findOne({
-      clerkId: clerkId,
+      clerkId: userId,
     });
 
     return user;
   } catch (error) {
     console.log('this error was in getuserbyId ->', error);
+  }
+}
+
+// actions for create user , delete user and update user
+export async function createUser(params: CreateUserParams) {
+  try {
+    connectToDatabase();
+
+    const user = await User.create(params);
+    return user;
+  } catch (error) {
+    console.log('the error occured in create user action ', error);
+  }
+}
+
+// yeh hogaya update ka !
+
+export async function UpdateUser(params: UpdateUserParams) {
+  try {
+    connectToDatabase();
+
+    const { clerkId, updateData, path } = params;
+
+    await User.findOneAndUpdate({ clerkId }, updateData, {
+      new: true,
+    });
+
+    revalidatePath(path);
+  } catch (error) {
+    console.log('the error is in update user ', error);
+  }
+}
+
+// yeh hogaya delete ka
+
+// !! need to go through the delete function kinda in doubt of how the algo is franed and the logic behind it
+
+export async function DeleteUser(params: DeleteUserParams) {
+  try {
+    connectToDatabase();
+
+    const { clerkId } = params;
+
+    const user = await User.findOneAndDelete({ clerkId });
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    // Delete user from database
+    // and questions, answers, comments, etc.
+
+    // get user question ids
+    // const userQuestionIds = await Question.find({ author: user._id}).distinct('_id');
+
+    // delete user questions
+    await Question.deleteMany({ author: user._id });
+
+    // TODO: delete user answers, comments, etc.
+
+    const deletedUser = await User.findByIdAndDelete(user._id);
+
+    return deletedUser;
+  } catch (error) {
+    console.log('this error is in delete user ', error);
   }
 }
